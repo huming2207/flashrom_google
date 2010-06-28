2c4a76c8d1fec5fc2e157b72ca384307739ea4bc
David Hendricks <dhendrix@chromium.org>
2010-06-28 14:00:43 -0700

    Add W25Q64 ranges
    This adds write protect ranges for the Winbond W25Q64 flash chip. Datasheet
    is at the following URL:
    http://www.winbond.com.tw/hq/enu/ProductAndSales/ProductLines/FlashMemory/SerialFlash/W25Q64BV.htm
    
    Review URL: http://codereview.chromium.org/2843013

--- writeprotect.c
+++ writeprotect.c
@@ -117,6 +117,37 @@ static struct w25q_range w25q80_ranges[] = {
 	{ 1, 1, 0x5, {0x000000, 32 * 1024} },
 };
 
+static struct w25q_range w25q64_ranges[] = {
+	{ X, X, 0, {0, 0} },	/* none */
+
+	{ 0, 0, 0x1, {0x7e0000, 128 * 1024} },
+	{ 0, 0, 0x2, {0x7c0000, 256 * 1024} },
+	{ 0, 0, 0x3, {0x780000, 512 * 1024} },
+	{ 0, 0, 0x4, {0x700000, 1024 * 1024} },
+	{ 0, 0, 0x5, {0x600000, 2048 * 1024} },
+	{ 0, 0, 0x6, {0x400000, 4096 * 1024} },
+
+	{ 0, 1, 0x1, {0x000000, 128 * 1024} },
+	{ 0, 1, 0x2, {0x000000, 256 * 1024} },
+	{ 0, 1, 0x3, {0x000000, 512 * 1024} },
+	{ 0, 1, 0x4, {0x000000, 1024 * 1024} },
+	{ 0, 1, 0x5, {0x000000, 2048 * 1024} },
+	{ 0, 1, 0x6, {0x000000, 4096 * 1024} },
+	{ X, X, 0x7, {0x000000, 8192 * 1024} },
+
+	{ 1, 0, 0x1, {0x7ff000, 4 * 1024} },
+	{ 1, 0, 0x2, {0x7fe000, 8 * 1024} },
+	{ 1, 0, 0x3, {0x7fc000, 16 * 1024} },
+	{ 1, 0, 0x4, {0x7f8000, 32 * 1024} },
+	{ 1, 0, 0x5, {0x7f8000, 32 * 1024} },
+
+	{ 1, 1, 0x1, {0x000000, 4 * 1024} },
+	{ 1, 1, 0x2, {0x000000, 8 * 1024} },
+	{ 1, 1, 0x3, {0x000000, 16 * 1024} },
+	{ 1, 1, 0x4, {0x000000, 32 * 1024} },
+	{ 1, 1, 0x5, {0x000000, 32 * 1024} },
+};
+
 struct w25q_status {
 	/* this maps to register layout -- do not change ordering */
 	unsigned char busy : 1;
@@ -155,8 +186,10 @@ static int w25_set_range(struct flashchip *flash,
 		w25q_ranges = w25q32_ranges;
 		num_entries = ARRAY_SIZE(w25q32_ranges);
 		break;
-//	case W_25Q64:
-//		break;
+	case W_25Q64:
+		w25q_ranges = w25q64_ranges;
+		num_entries = ARRAY_SIZE(w25q64_ranges);
+		break;
 	default:
 		msg_cerr("%s: flash chip mismatch, aborting\n", __func__);
 		return -1;
