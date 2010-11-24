d7c4bd5a626df2768becc0889fd7c07f32fcb726
David Hendricks <dhendrix@chromium.org>
2010-11-24 11:00:40 -0800

    Add new testing framework along with a few micro-tests.
    This is currently a work-in-progress. However, I have not pestered Louis with
    a code review in a while, which means it is time to dump hundreds of lines of
    indecipherable shell code in his lap :-)
    
    Included in this patch are several micro-tests:
    do_tests.sh: the unit test framework
    chip_size.sh: test the ability to print the chip's size
    wp_range.sh: test the ability to set the chip's write protect range
    wp_toggle.sh: test the ability to turn write protection on or off
    partial_writes_x86_bios.sh: test partial writes -- ONLY FOR x86 BIOS, DO NOT USE ON EC
    partial_writes_ec.sh: test partial writes for EC (it8500 used for testing, may not work on all ECs)
    
    TODO:
    1. The partial_writes tests are pretty basic now. We should do it twice -- once to ensure the blocks actually get written (this may require code changes to flashrom), and once again to ensure regions get skipped appropriately.
    2. We should make better use of the SPI emulator.
    
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/5136001

---
+++ util/wp-toggle.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+#
+# Copyright (C) 2010 Google Inc.
+# Written by David Hendricks for Google Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+# Assume we can set enable/disable bits (that is, SPI chip is not hardware
+# write-protected). Also, assume output is in the form:
+# WP: write protect is enabled
+# WP: write protect is disabled
+#
+# FIXME: Carl-Daniel pointed out that this script does little to validate that
+# the settings on the chip are actually correct. It merely checks that Flashrom
+# prints what we want it to print.
+
+
+LOGFILE="${0}.log"
+WP_DISABLED=0
+WP_ENABLED=1
+MAGIC="write protect is"
+
+wp_toggle_fail()
+{
+	echo "$1" >> ${LOGFILE}
+	echo "$0: failed" >> ${LOGFILE}
+	exit ${EXIT_FAILURE}
+}
+
+wp_status()
+{
+	if [ "$(printf %s\\n "$1" | sed 's/.*enabled.*/enabled/')" = "enabled" ]; then
+		return ${WP_ENABLED} 
+	elif [ "$(printf %s\\n "$1" | sed 's/.*disabled.*/disabled/')" = "disabled" ]; then
+		return ${WP_DISABLED}
+	else
+		wp_toggle_fail "unknown write protect status: \"$1\""
+	fi
+}
+
+# Back-up old settings
+tmp=$(./flashrom ${FLASHROM_PARAM} --wp-status 2>/dev/null | grep "$MAGIC")
+wp_status "$tmp"
+old_status=$?
+echo "old write protect status: ${old_status}" >> ${LOGFILE}
+
+# invert the old setting
+if [ ${old_status} -eq ${WP_ENABLED} ]; then
+	./flashrom ${FLASHROM_PARAM} --wp-disable 2>/dev/null
+	tmp=$(./flashrom ${FLASHROM_PARAM} --wp-status 2>/dev/null | grep "$MAGIC")
+	wp_status "$tmp"
+	if [ $? = ${WP_ENABLED} ]; then
+		wp_toggle_fail "failed to disable write protection"
+	fi
+elif [ ${old_status} -eq ${WP_DISABLED} ]; then
+	./flashrom ${FLASHROM_PARAM} --wp-enable 2>/dev/null
+	tmp=$(./flashrom ${FLASHROM_PARAM} --wp-status 2>/dev/null | grep "$MAGIC")
+	wp_status "$tmp"
+	if [ $? = ${WP_DISABLED} ]; then
+		wp_toggle_fail "failed to enable write protection"
+	fi
+fi
+
+# restore old setting
+if [ ${old_status} -eq ${WP_ENABLED} ]; then
+	./flashrom ${FLASHROM_PARAM} --wp-enable 2>/dev/null
+	tmp=$(./flashrom ${FLASHROM_PARAM} --wp-status 2>/dev/null | grep "$MAGIC")
+	wp_status "$tmp"
+	if [ $? != ${WP_ENABLED} ]; then
+		wp_toggle_fail "failed to enable write protection"
+	fi
+elif [ ${old_status} -eq ${WP_DISABLED} ]; then
+	./flashrom ${FLASHROM_PARAM} --wp-disable 2>/dev/null
+	tmp=$(./flashrom ${FLASHROM_PARAM} --wp-status 2>/dev/null | grep "$MAGIC")
+	wp_status "$tmp"
+	if [ $? != ${WP_DISABLED} ]; then
+		wp_toggle_fail "failed to disable write protection"
+	fi
+fi
+
+echo "$0: passed" >> ${LOGFILE}
+return ${EXIT_SUCCESS}
