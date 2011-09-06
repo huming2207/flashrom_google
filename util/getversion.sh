dc212ddf851e34f916a8f21cdf920c3e3b0c9651
David Hendricks <dhendrix@chromium.org>
2011-09-06 16:33:39 -0700

    make getversion script more generic
    This change completely re-vamps the getversion script. The idea is to make it
    more generic and adaptable for other applications.
    
    There are now three functions for each supported SCM (Git and Subversion)
    which query revision, server, and timestamp information. Some logic toward
    the bottom of the script parses command-line arguments (e.g. -r, -s, and -t)
    which allow the user to directly control which information is retrieved.
    
    In summary, this patch does the following:
    - Add command-line parsing (-r | --revision, -s | --server, -t | --timestamp).
    - Add SVN functions for getting timestamp and upstream server info.
    - Breaks Git functionality down into constituent parts.
    - Adds trivial wrappers which select git/svn functions.
    
    BUG=none
    TEST=See notes below
    
    Git repo without uncommitted changes:
    $ ./util/getversion.sh -r -s -t
    f053479
    git.chromium.org/chromiumos/third_party/flashrom.git
    Sep 06 2011 23:33:39 UTC
    
    Git repo with uncommitted changes:
    $ ./util/getversion.sh -r -s -t
    0aedacf
    git.chromium.org/chromiumos/third_party/flashrom.git
    Sep 06 2011 16:44:20 +
    
    Unmodified Subversion repo:
    $ ./getversion.sh -r -s -t
    r367
    https://mosys.googlecode.com/svn/trunk
    Aug 27 2011 00:05:46 UTC
    
    Subversion repo with local modifications:
    $ ./getversion.sh -r -s -t
    r367
    https://mosys.googlecode.com/svn/trunk
    Sep 06 2011 16:49:11 +
    
    Change-Id: I06523508a660d03eda945d06f1bd6068564aff7a
    Reviewed-on: http://gerrit.chromium.org/gerrit/7306
    Tested-by: David Hendricks <dhendrix@chromium.org>
    Reviewed-by: David Hendricks <dhendrix@chromium.org>

--- util/getversion.sh
+++ util/getversion.sh
@@ -1,93 +1,167 @@
 #!/bin/sh
-##
-## This file is part of the flashrom project.
-##
-## Copyright (C) 2010 Chromium OS Authors
-##
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program; if not, write to the Free Software
-## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
-##
+#
+# This file is part of the flashrom project.
+#
+# Copyright (C) 2011 Chromium OS Authors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# getversion.sh: Get version / revision info. Currently, only Git and Subversion
+# are supported.
+#
+# *_revision:   Echo the latest source code revision (git hash or svn revision).
+#
+# *_server:     Echo the server from where the source code was checked out.
+#
+# *_timestamp:  Echo the timestamp of most recent modification. If the sources
+#               are pristine, the timestamp will correspond to the most recently
+#               committed change, expressed in UTC. If there are local
+#               modifications, the timestamp will correspond to compilation
+#               time and a '+' is added to denote uncommitted changes.
+#
 
-# Rerieve subversion version mumber if available.
-subv_version() {
-    LC_ALL=C
+git_revision() {
+  echo $(git log --pretty=format:'%h' -n 1)
+}
 
-    svnversion -cn . 2>/dev/null | \
-	sed -e "s/.*://" -e "s/\([0-9]*\).*/\1/" | \
-	grep "[0-9]" || LC_ALL=C svn info . 2>/dev/null | \
-	awk '/^Revision:/ {print $$2 }' | \
-	grep "[0-9]" || LC_ALL=C git svn info . 2>/dev/null | \
-	awk '/^Revision:/ {print $$2 }' | \
-	grep "[0-9]" || echo ''
+git_server() {
+  # Note: This may not work as expected if multiple remotes are fetched from.
+  echo $(git remote -v | \
+         awk '/fetch/ {split($2, pieces, "//"); print pieces[2]; exit 0}')
+}
+
+git_timestamp() {
+  local date_format="+%b %d %Y %H:%M:%S"
+  local timestamp
+
+  # are there local changes in the client?
+  if git status | \
+    egrep '^# Change(s to be committed|d but not updated):$' > /dev/null
+  then
+    timestamp=$(date "${date_format} +")
+  else
+    # No local changes, get date of the last log record.
+    timestamp=$(git log --pretty=format:'%ct' -n 1 | \
+                awk '{print strftime("%b %d %Y %H:%M:%S UTC", $1, 1)}')
+  fi
+
+  echo "${timestamp}"
+}
+
+svn_revision() {
+  local revision=""
+
+  # Try both "svnversion" and "svn info"
+  revision=$(LC_ALL=C svnversion -cn . 2>/dev/null | \
+             sed -e "s/.*://" -e "s/\([0-9]*\).*/\1/" | grep "[0-9]")
+
+  if [ -z "${revision}" ]; then
+    revision=$(LC_ALL=C svn info . 2>/dev/null | \
+               awk '/^Revision:/ {print $2 }' | grep "[0-9]")
+  fi
+
+  # Prepend revision with 'r'
+  echo "r${revision}"
+}
+
+svn_server() {
+  echo $(LC_ALL=C svn info . 2>/dev/null | awk '/^URL:/ {print $2 }')
+}
+
+svn_timestamp() {
+  local date_format="+%b %d %Y %H:%M:%S"
+  local timestamp
+
+  # Whitelist most of the characters that appear on the left of
+  # "svn status" output, with the notable exceptions being '?' (Not
+  # under version control) and 'X'(item is an externals definition)
+  if [ -n "$(LC_ALL=C svn status . | grep '^ *[ADMRCI\!L+SKOTB\*].*')" ]; then
+    timestamp=$(date "${date_format} +")
+  else
+    local last_commit_date=$(svn info . | \
+      grep "Last Changed Date:" | awk '{print $4" "$5" "$6}')
+    timestamp=$(date --utc --date "${last_commit_date}" "${date_format} UTC")
+  fi
+
+  echo "${timestamp}"
 }
 
 #
-# Generate git version string which has the following format:
+# Determine which SCM we're using and choose the appropriate functions
 #
-#    <git server> : <commit hash> : <timestamp>
-#
-# In case the local tree is pristine, the timesatmp is derived from the top
-# log message and is expressed in UTC.
-#
-# In case the local tree has modifications, the timestamp is the actual
-# compilation time, this is indicated by adding a '+' to the end of the
-# timestamp.
-#
-git_version() {
-    local date_format="+%b %d %Y %H:%M:%S"
-    local timestamp
+IS_GIT=0
+IS_SVN=0
 
-    if [ ! -d .git ]; then
-	echo ''
-	return
-    fi
-
-    # are there local changes in the client?
-    if  git status | \
-	egrep '^# Change(s to be committed|d but not updated):$' > /dev/null
-    then
-        timestamp=$(date "${date_format} +")
-    else
-	# No local changes, get date of the last log record.
-	local last_commit_date=$(git log | head -3 | grep '^Date:' | \
-	    awk '{print $3" "$4" "$6" "$5" "$7}')
-	timestamp=$(date --utc --date "${last_commit_date}" \
-	    "${date_format} UTC")
-    fi
-    # Get remote server name (only the first line of `git remote' is
-    # considered.
-    local server=$(git remote -v |\
-        awk '{
-             if (!server) {
-                server = $2;
-                split(server, pieces, "//");
-                print pieces[2] }
-        }')
-
-    # Get the commit hash.
-    local commit=$(git log --oneline | head -1 | awk '{print $1}')
-
-    # Generate the version string.
-    echo "${server} : ${commit} : ${timestamp}"
-}
-
-v=$(subv_version)
-
-if [ -z "${v}" ]; then
-    v=$(git_version)
-else
-    # Make SVN version look like it did before git option was added.
-    v="-r${v}"
+if [ -d ".git" ]; then
+  IS_GIT=1
+elif [ -d ".svn" ]; then
+  IS_SVN=1
 fi
-echo "${v}"
+
+get_revision() {
+  if [ $IS_GIT -eq 1 ]; then
+    echo "$(git_revision)"
+  elif [ $IS_SVN -eq 1 ]; then
+    echo "$(svn_revision)"
+  fi
+}
+
+get_server() {
+  if [ $IS_GIT -eq 1 ]; then
+    echo "$(git_server)"
+  elif [ $IS_SVN -eq 1 ]; then
+    echo "$(svn_server)"
+  fi
+}
+
+get_timestamp() {
+  if [ $IS_GIT -eq 1 ]; then
+    echo "$(git_timestamp)"
+  elif [ $IS_SVN -eq 1 ]; then
+    echo "$(svn_timestamp)"
+  fi
+}
+
+show_help() {
+  echo "Usage: getversion.sh <options>"
+  echo ""
+  echo "Options:"
+  echo -e "\t-r | --revision     Revision (SVN), commit hash (Git)"
+  echo -e "\t-s | --server       Upstream server"
+  echo -e "\t-t | --timestamp    UTC timestamp of most recent modification"
+}
+
+if [ -z "$*" ]; then
+  # default action: print "<revision> : <server> : <timestamp>"
+  echo "${0}: Using default parameters" >&2
+  echo "$(get_server) : $(get_revision) : $(get_timestamp)"
+else
+  for ARG in $@; do
+    case ${ARG} in
+      -h|--help)
+        show_help;
+        shift;;
+      -r|--revision)
+        echo "$(get_revision)"
+        shift;;
+      -s|--server)
+        echo "$(get_server)"
+        shift;;
+      -t|--timestamp)
+        echo "$(get_timestamp)"
+        shift;;
+    esac;
+  done
+fi
