94a81467cc157cbe56ed526f7c213b051ffd2264
David Hendricks <dhendrix@chromium.org>
2011-11-11 14:55:43 -0800

    Fix use_big_lock.sh
    This updates the script so that it echos the return code of the compilation,
    and does not actually attempt to run the test file. This helps in situations
    where the host cannot run the test file (e.g. amd64 host, ARM target).
    
    BUG=none
    TEST=Built on amd64 and tested binary on aebl
    
    Change-Id: Ia5f424dac7fec344c25274a3b2c75f12b63c1606
    Reviewed-on: https://gerrit.chromium.org/gerrit/11562
    Reviewed-by: Yung-Chieh Lo <yjlou%chromium.org@gtempaccount.com>
    Tested-by: David Hendricks <dhendrix@chromium.org>

--- util/use_big_lock.sh
+++ util/use_big_lock.sh
@@ -14,14 +14,14 @@ int main()\
 #ifdef _POSIX_C_SOURCE
 	if ((long)_POSIX_C_SOURCE >= 200112)
 	        exit(EXIT_SUCCESS);
+#else
+#error
 #endif
-        exit (EXIT_FAILURE);
+        exit(EXIT_FAILURE);
 }
 " > .test.c
 
-${CC} -o .test .test.c && ./.test || exit 1
+${CC} -o .test .test.c
 rc=$?
 echo $rc
-
-rm -f .test
-exit $rc
+exit
