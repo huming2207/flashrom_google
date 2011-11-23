elecena/flashrom

chipset_enable.c: add a message for untested chipset enables
old output:
Calibrating delay loop... OK.
Found chipset "Intel QS57", enabling flash write... OK.
This chipset supports the following protocols: FWH, SPI.

new non-verbose output for tested chipsets:
Calibrating delay loop... OK.
Found chipset "Intel QS57". Enabling flash write... OK.
This chipset supports the following protocols: FWH, SPI.

new non-verbose output for untested chipsets:
Found chipset "Intel QS57".
This chipset is marked as untested. If you are using an up-to-date version
of flashrom please email a report to flashrom@flashrom.org including a
verbose (-V) log. Thank you!
Enabling flash write... OK.
This chipset supports the following protocols: FWH, SPI.

Signed-off-by: Stefan Tauner <stefan.tauner@student.tuwien.ac.at>
Acked-by: Uwe Hermann <uwe@hermann-uwe.de>

chipset_enable.c
@@ -1226,13 +1226,22 @@ int chipset_flash_enable(void)
 					chipset_enables[i].device_name);
 			continue;
 		}
-		msg_pinfo("Found chipset \"%s %s\", enabling flash write... ",
-		       chipset_enables[i].vendor_name,
-		       chipset_enables[i].device_name);
-		msg_pdbg("chipset PCI ID is %04x:%04x, ",
+		msg_pinfo("Found chipset \"%s %s\"",
+			  chipset_enables[i].vendor_name,
+			  chipset_enables[i].device_name);
+		msg_pdbg("with PCI ID %04x:%04x",
 			 chipset_enables[i].vendor_id,
 			 chipset_enables[i].device_id);
+		msg_pinfo(". ");
 
+		if (chipset_enables[i].status == NT) {
+			msg_pinfo("\nThis chipset is marked as untested. If "
+				  "you are using an up-to-date version\nof "
+				  "flashrom please email a report to "
+				  "flashrom@flashrom.org including a\nverbose "
+				  "(-V) log. Thank you!\n");
+		}
+		msg_pinfo("Enabling flash write... ");
 		ret = chipset_enables[i].doit(dev,
 					      chipset_enables[i].device_name);
 		if (ret == NOT_DONE_YET) {

huming2207/flashrom_google

git-svn-id: svn://coreboot.org/flashrom/trunk@1379 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

chipset_enable.c
@@ -1244,13 +1244,22 @@ int chipset_flash_enable(void)
 					chipset_enables[i].device_name);
 			continue;
 		}
-		msg_pdbg("Found chipset \"%s %s\", enabling flash write... ",
-		       chipset_enables[i].vendor_name,
-		       chipset_enables[i].device_name);
-		msg_pdbg("chipset PCI ID is %04x:%04x, ",
+		msg_pdbg("Found chipset \"%s %s\"",
+			  chipset_enables[i].vendor_name,
+			  chipset_enables[i].device_name);
+		msg_pdbg("with PCI ID %04x:%04x",
 			 chipset_enables[i].vendor_id,
 			 chipset_enables[i].device_id);
+		msg_pinfo(". ");
 
+		if (chipset_enables[i].status == NT) {
+			msg_pinfo("\nThis chipset is marked as untested. If "
+				  "you are using an up-to-date version\nof "
+				  "flashrom please email a report to "
+				  "flashrom@flashrom.org including a\nverbose "
+				  "(-V) log. Thank you!\n");
+		}
+		msg_pinfo("Enabling flash write... ");
 		ret = chipset_enables[i].doit(dev,
 					      chipset_enables[i].device_name);
 		if (ret == NOT_DONE_YET) {