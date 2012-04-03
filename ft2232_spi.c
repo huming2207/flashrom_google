97f259ff09c161315d086c25f0860adc12ce075c
Todd Broch <tbroch@chromium.org>
2012-04-03 15:52:14 -0700

    flashrom: ft2232_spi: Add support for servo V2 legacy spi support.
    servo V2 supports up to three separate SPI programming channels.
    Previous commit, e1e13ec5fce5821e4feeb15b97288836e5b24621, covered the
    two new ports.  This CL adds the original 'legacy' support found on
    servo V1.
    
    BUG=none
    TEST=manual,
    
    Successfully communicate with EEPROM using
      sudo flashrom -V -p ft2232_spi:type=servo-v2-legacy
    
    Change-Id: I540ad2d304dc69a7c79ca154beb744ef947ff808
    Reviewed-on: https://gerrit.chromium.org/gerrit/19556
    Reviewed-by: Mark Hayter <mdhayter@chromium.org>
    Reviewed-by: David Hendricks <dhendrix@chromium.org>
    Commit-Ready: Todd Broch <tbroch@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>

--- ft2232_spi.c
+++ ft2232_spi.c
@@ -51,7 +51,8 @@
 
 #define GOOGLE_VID		0x18D1
 #define GOOGLE_SERVO_PID	0x5001
-#define GOOGLE_SERVO_V2_PID	0x5003
+#define GOOGLE_SERVO_V2_PID0	0x5002
+#define GOOGLE_SERVO_V2_PID1	0x5003
 
 const struct usbdev_status devs_ft2232spi[] = {
 	{FTDI_VID, FTDI_FT2232H_PID, OK, "FTDI", "FT2232H"},
@@ -66,7 +67,8 @@ const struct usbdev_status devs_ft2232spi[] = {
 	{OLIMEX_VID, OLIMEX_ARM_OCD_H_PID, NT, "Olimex", "ARM-USB-OCD-H"},
 	{OLIMEX_VID, OLIMEX_ARM_TINY_H_PID, NT, "Olimex", "ARM-USB-TINY-H"},
 	{GOOGLE_VID, GOOGLE_SERVO_PID, OK, "Google", "Servo"},
-	{GOOGLE_VID, GOOGLE_SERVO_V2_PID, OK, "Google", "Servo V2"},
+	{GOOGLE_VID, GOOGLE_SERVO_V2_PID0, OK, "Google", "Servo V2 Legacy"},
+	{GOOGLE_VID, GOOGLE_SERVO_V2_PID1, OK, "Google", "Servo V2"},
 	{},
 };
 
@@ -226,7 +228,11 @@ int ft2232_spi_init(void)
 			ft2232_interface = INTERFACE_A;
 		} else if (!strcasecmp(arg, "servo-v2")) {
 			ft2232_vid = GOOGLE_VID;
-			ft2232_type = GOOGLE_SERVO_V2_PID;
+			ft2232_type = GOOGLE_SERVO_V2_PID1;
+			ft2232_interface = INTERFACE_A;
+		} else if (!strcasecmp(arg, "servo-v2-legacy")) {
+			ft2232_vid = GOOGLE_VID;
+			ft2232_type = GOOGLE_SERVO_V2_PID0;
 			ft2232_interface = INTERFACE_A;
 		} else {
 			msg_perr("Error: Invalid device type specified.\n");
