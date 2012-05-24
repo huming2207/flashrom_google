ee71247287d9aeb082325b9cd8fe114d3180526f
David Hendricks <dhendrix@chromium.org>
2012-05-23 21:50:59 -0700

    Update BSD-licensed files with appropriate header
    Upstream flashrom is GPLv2, but modules from Google are typically
    released under a BSD or BSD-like license. This patch applies the
    correct license header to the gec.c and gec_lpc_commands.h files.
    
    BUG=none
    TEST=none
    
    Change-Id: Ia7b11b1837b66ff1c2ececcbe8545f44ac529dd9
    Reviewed-on: https://gerrit.chromium.org/gerrit/23468
    Reviewed-by: Yung-Chieh Lo <yjlou%chromium.org@gtempaccount.com>
    Commit-Ready: David Hendricks <dhendrix@chromium.org>
    Tested-by: David Hendricks <dhendrix@chromium.org>

--- gec.c
+++ gec.c
@@ -1,6 +1,36 @@
-// Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+/*
+ * This file is part of the flashrom project.
+ *
+ * Copyright (C) 2012 The Chromium OS Authors. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of Google or the names of contributors or
+ * licensors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * This software is provided "AS IS," without a warranty of any kind.
+ * ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES,
+ * INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED.
+ * GOOGLE INC AND ITS LICENSORS SHALL NOT BE LIABLE
+ * FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING
+ * OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.  IN NO EVENT WILL
+ * GOOGLE OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA,
+ * OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR
+ * PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF
+ * LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE,
+ * EVEN IF GOOGLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
