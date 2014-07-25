13343b87ee0d547003fabe14b61b15b3d5ba24bf
David Riley <davidriley@chromium.org>
2014-07-25 11:37:56 -0700

    flashrom: Properly convert 32-bit pointers to u64_t.
    For arm64 with 32-bit userspace, pointers such as 0xff96ebf8 were
    incorrectly getting converted to u64_t 0xffffffffff96ebf8 in the
    spi_ioc_transfer which was causing ioctl()s to be rejected by
    the kernel.
    
    BUG=chrome-os-partner:30795
    BRANCH=rush_ryu
    TEST='flashrom -V' no longer reports all ioctls as failing with EFAULT
    
    Change-Id: I5a15b4ca5d9657c3cb1ddccd42eafd91c852dd26
    Signed-off-by: David Riley <davidriley@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/209910
    Reviewed-by: David Hendricks <dhendrix@chromium.org>

--- linux_spi.c
+++ linux_spi.c
@@ -214,11 +214,11 @@ static int linux_spi_send_command(unsigned int writecnt, unsigned int readcnt,
 	int msg_start = 0, msg_count = 0;
 	struct spi_ioc_transfer msg[2] = {
 		{
-			.tx_buf = (uint64_t)(ptrdiff_t)txbuf,
+			.tx_buf = (uint64_t)(uintptr_t)txbuf,
 			.len = writecnt,
 		},
 		{
-			.rx_buf = (uint64_t)(ptrdiff_t)rxbuf,
+			.rx_buf = (uint64_t)(uintptr_t)rxbuf,
 			.len = readcnt,
 		},
 	};
