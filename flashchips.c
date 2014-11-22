8401b92c52d1c36ecf3ff20a072102988328a7e8
David Hendricks <dhendrix@chromium.org>
2014-11-21 16:45:08 -0800

    Initial s25fl116k support
    Quick 'n dirty patch to (hopefully) make this chip readable/writeable.
    
    BUG=none
    BRANCH=none
    TEST=with the appropriate overlay for servo:
    
     $ sudo flashrom -p ft2232_spi:type=servo-v2 --flash-name
    flashrom v0.9.4  :  :  on Linux 3.13.0-39-generic (x86_64), built with libpci 3.1.10, GCC 4.9.x-google 20140827 (prerelease), little endian
    vendor="Spansion" name="S25FL116K"
    
    reads and writes also work.
    
    Signed-off-by: David Hendricks <dhendrix@chromium.org>
    Change-Id: Ie9907c8756d3f6324300335c025d7bb1496666e4
    Reviewed-on: https://chromium-review.googlesource.com/231449
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
    Commit-Queue: Vadim Bendebury <vbendeb@chromium.org>

--- flashchips.c
+++ flashchips.c
@@ -6503,6 +6503,37 @@ const struct flashchip flashchips[] = {
 
 	{
 		.vendor		= "Spansion",
+		.name		= "S25FL116K",
+		.bustype	= BUS_SPI,
+		.manufacture_id	= SPANSION_ID,
+		.model_id	= SPANSION_S25FL116K,
+		.total_size	= 2048,
+		.page_size	= 256,
+		.tested		= TEST_OK_PREW,
+		.probe		= probe_spi_rdid,
+		.probe_timing	= TIMING_ZERO,
+		.block_erasers	=
+		{
+			{
+				.eraseblocks = { {4 * 1024, 512} },
+				.block_erase = spi_block_erase_20,
+			}, {
+				.eraseblocks = { {64 * 1024, 32} },
+				.block_erase = spi_block_erase_d8,
+			}, {
+				.eraseblocks = { {2 * 1024 * 1024, 1} },
+				.block_erase = spi_block_erase_c7,
+			}
+		},
+		.unlock		= spi_disable_blockprotect,
+		.write		= spi_chip_write_256,
+		.read		= spi_chip_read,
+		.voltage	= {2700, 3600},
+		/* TODO: write-protection */
+	},
+
+	{
+		.vendor		= "Spansion",
 		.name		= "S25FS128S Large Sectors",
 		.bustype	= BUS_SPI,
 		.manufacture_id	= SPANSION_ID,
