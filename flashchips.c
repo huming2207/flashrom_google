9356d1638b87cf4124236b296224d88bb692ec3f
David Hendricks <dhendrix@chromium.org>
2015-03-06 14:07:25 -0800

    Revert "HACK: pretend W25Q128 is 8MB"
    This reverts commit 8694b57b8277488e2808656f5e63ed79612b12c4.
    
    The hack is no longer needed and is interfering with development of
    more modern platforms.
    
    BUG=chrome-os-partner:22419
    BRANCH=none
    TEST=compile tested only
    
    Change-Id: I9b49d0b56d35a654224a9fc008d2393b119a67a6
    Signed-off-by: David Hendricks <dhendrix@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/257081
    Reviewed-by: Kevin K Wong <kevin.k.wong@intel.com>
    Reviewed-by: Shawn N <shawnn@chromium.org>

--- flashchips.c
+++ flashchips.c
@@ -9406,7 +9406,7 @@ const struct flashchip flashchips[] = {
 		.bustype	= BUS_SPI,
 		.manufacture_id	= WINBOND_NEX_ID,
 		.model_id	= WINBOND_NEX_W25Q128,
-		.total_size	= 16384/2,
+		.total_size	= 16384,
 		.page_size	= 256,
 		.feature_bits	= FEATURE_WRSR_WREN,
 		.tested		= TEST_OK_PROBE,
@@ -9415,19 +9415,19 @@ const struct flashchip flashchips[] = {
 		.block_erasers	=
 		{
 			{
-				.eraseblocks = { {4 * 1024, 4096/2} },
+				.eraseblocks = { {4 * 1024, 4096} },
 				.block_erase = spi_block_erase_20,
 			}, {
-				.eraseblocks = { {32 * 1024, 512/2} },
+				.eraseblocks = { {32 * 1024, 512} },
 				.block_erase = spi_block_erase_52,
 			}, {
-				.eraseblocks = { {64 * 1024, 256/2} },
+				.eraseblocks = { {64 * 1024, 256} },
 				.block_erase = spi_block_erase_d8,
 			}, {
-				.eraseblocks = { {16 * 1024 * 1024 / 2, 1} },
+				.eraseblocks = { {16 * 1024 * 1024, 1} },
 				.block_erase = spi_block_erase_60,
 			}, {
-				.eraseblocks = { {16 * 1024 * 1024 / 2, 1} },
+				.eraseblocks = { {16 * 1024 * 1024, 1} },
 				.block_erase = spi_block_erase_c7,
 			}
 		},
