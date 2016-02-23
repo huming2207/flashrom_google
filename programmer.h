3d3b3994ffef79d44cbf819c188873b2bf55deff
David Hendricks <dhendrix@chromium.org>
2016-02-23 23:22:28 +0000

    Revert "Implement status register read/write callbacks in flash struct"
    This reverts commit 953967b97cd6a02f6c7bb75a1dffed24ce3d1710.
    
    We seem to have overlooked a few things:
    https://bugs.chromium.org/p/chromium/issues/detail?id=588592
    https://code.google.com/p/chrome-os-partner/issues/detail?id=50429
    
    Change-Id: Ibd38281cfafbcbc9988b2bd0d2134aab09239d6d
    Reviewed-on: https://chromium-review.googlesource.com/329135
    Commit-Ready: David Hendricks <dhendrix@chromium.org>
    Tested-by: David Hendricks <dhendrix@chromium.org>
    Reviewed-by: David Hendricks <dhendrix@chromium.org>
    Reviewed-by: danny chan <dchan@chromium.org>

--- programmer.h
+++ programmer.h
@@ -631,8 +631,6 @@ struct spi_programmer {
 	/* Optimized functions for this programmer */
 	int (*read)(struct flashchip *flash, uint8_t *buf, unsigned int start, unsigned int len);
 	int (*write_256)(struct flashchip *flash, uint8_t *buf, unsigned int start, unsigned int len);
-	uint8_t (*read_status)(const struct flashchip *flash);
-	int (*write_status)(const struct flashchip *flash, int status);
 };
 
 extern const struct spi_programmer *spi_programmer;
@@ -701,8 +699,6 @@ struct opaque_programmer {
 	int (*read) (struct flashchip *flash, uint8_t *buf, unsigned int start, unsigned int len);
 	int (*write) (struct flashchip *flash, uint8_t *buf, unsigned int start, unsigned int len);
 	int (*erase) (struct flashchip *flash, unsigned int blockaddr, unsigned int blocklen);
-	uint8_t (*read_status) (const struct flashchip *flash);
-	int (*write_status) (const struct flashchip *flash, int status);
 	const void *data;
 };
 extern struct opaque_programmer *opaque_programmer;
