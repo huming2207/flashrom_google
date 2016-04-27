59543cd1371071a950befaf0a123b2f4f0b807c8
David Schneider <dnschneid@chromium.org>
2016-04-27 02:11:00 -0700

    Add support for GigaDevice GD25LQ40
    TEST=writing and verifying GD25LQ40 works
    BRANCH=none
    BUG=none
    
    Change-Id: Ib7757701e54f31aca4492dcfaf46b5ba5088e505
    Signed-off-by: David Schneider <dnschneid@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/340897
    Tested-by: Caesar Wang <wxt@rock-chips.com>
    Reviewed-by: David Hendricks <dhendrix@chromium.org>
    Reviewed-by: Patrick Georgi <patrick@georgi-clan.de>

--- flashchips.c
+++ flashchips.c
@@ -4009,6 +4009,43 @@ const struct flashchip flashchips[] = {
 
 	{
 		.vendor		= "GigaDevice",
+		.name		= "GD25LQ40",
+		.bustype	= BUS_SPI,
+		.manufacture_id	= GIGADEVICE_ID,
+		.model_id	= GIGADEVICE_GD25LQ40,
+		.total_size	= 512,
+		.page_size	= 256,
+		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_UNBOUND_READ,
+		.tested		= TEST_UNTESTED,
+		.probe		= probe_spi_rdid,
+		.probe_timing	= TIMING_ZERO,
+		.block_erasers	=
+		{
+			{
+				.eraseblocks = { {4 * 1024, 128} },
+				.block_erase = spi_block_erase_20,
+			}, {
+				.eraseblocks = { {32 * 1024, 16} },
+				.block_erase = spi_block_erase_52,
+			}, {
+				.eraseblocks = { {64 * 1024, 8} },
+				.block_erase = spi_block_erase_d8,
+			}, {
+				.eraseblocks = { {512 * 1024, 1} },
+				.block_erase = spi_block_erase_60,
+			}, {
+				.eraseblocks = { {512 * 1024, 1} },
+				.block_erase = spi_block_erase_c7,
+			}
+		},
+		.unlock		= spi_disable_blockprotect,
+		.write		= spi_chip_write_256,
+		.read		= spi_chip_read,
+		.voltage        = {1650, 1950},
+	},
+
+	{
+		.vendor		= "GigaDevice",
 		.name		= "GD25Q80",
 		.bustype	= BUS_SPI,
 		.manufacture_id	= GIGADEVICE_ID,
