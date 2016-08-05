c49783d9a532ba02de867637bc9382fe40cfdb36
Carl-Daniel Hailfinger <c-d.hailfinger.devel.2006@gmx.net>
2016-08-05 10:52:06 -0700

    SPI command blacklisting/ignorelisting for flash chip emulator in dummyflasher
    Corresponds to r1490 from upstream.
    From upstream commit log:
    Usage:
    flashrom -p dummy:spi_blacklist=commandlist
    flashrom -p dummy:spi_ignorelist=commandlist
    
    If commandlist is 0302, flashrom will refuse (blacklist) or ignore
    (ignorelist) command 0x03 (READ) and command 0x02 (WRITE). The
    commandlist can be up to 512 bytes (256 commands) long.
    Specifying flash chip emulation is a good idea to get useful results.
    
    Very useful for testing corner cases if you don't own a locked down
    Intel chipset and want to simulate such a thing.
    
    Example usage:
    dd if=/dev/zeros bs=1024k count=4 of=dummy_simulator.rom
    dd if=/dev/urandom bs=1024k count=4 of=randomimage.rom
    flashrom -p dummy:emulate=SST25VF032B,image=dummy_simulator.rom,\
    spi_blacklist=20,spi_ignorelist=52 -w randomimage.rom -V
    
    BUG=chromium:478356
    BRANCH=none
    TEST=none
    
    Change-Id: Idc964e7fb4900c5ebbc559278e73a48c308093f1
    Signed-off-by: Souvik Ghosh <souvikghosh@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/366810
    Commit-Ready: David Hendricks <dhendrix@chromium.org>
    Reviewed-by: David Hendricks <dhendrix@chromium.org>

--- dummyflasher.c
+++ dummyflasher.c
@@ -20,6 +20,8 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 #include "flash.h"
 #include "chipdrivers.h"
 #include "programmer.h"
@@ -42,6 +44,10 @@
  *   flashrom -p dummy:emulate=VARIABLE_SIZE,size=4194304
  */
 #define VARIABLE_SIZE_CHIP_NAME "VARIABLE_SIZE"
+unsigned char spi_blacklist[256];
+unsigned char spi_ignorelist[256];
+int spi_blacklist_size = 0;
+int spi_ignorelist_size = 0;
 #endif
 #endif
 
@@ -146,6 +152,7 @@ int dummy_init(void)
 {
 	char *bustext = NULL;
 	char *tmp = NULL;
+	int i;
 #if EMULATE_CHIP
 	struct stat image_stat;
 #if EMULATE_SPI_CHIP
@@ -193,6 +200,78 @@ int dummy_init(void)
 			return 1;
 		}
 	}
+
+	tmp = extract_programmer_param("spi_blacklist");
+	if (tmp) {
+		i = strlen(tmp);
+		if (!strncmp(tmp, "0x", 2)) {
+			i -= 2;
+			memmove(tmp, tmp + 2, i + 1);
+		}
+		if ((i > 512) || (i % 2)) {
+			msg_perr("Invalid SPI command blacklist length\n");
+			free(tmp);
+			return 1;
+		}
+		spi_blacklist_size = i / 2;
+		for (i = 0; i < spi_blacklist_size * 2; i++) {
+			if (!isxdigit((unsigned char)tmp[i])) {
+				msg_perr("Invalid char \"%c\" in SPI command "
+					 "blacklist\n", tmp[i]);
+				free(tmp);
+				return 1;
+			}
+		}
+		for (i = 0; i < spi_blacklist_size; i++) {
+			unsigned int tmp2;
+			/* SCNx8 is apparently not supported by MSVC (and thus
+			 * MinGW), so work around it with an extra variable
+			 */
+			sscanf(tmp + i * 2, "%2x", &tmp2);
+			spi_blacklist[i] = (uint8_t)tmp2;
+		}
+		msg_pdbg("SPI blacklist is ");
+		for (i = 0; i < spi_blacklist_size; i++)
+			msg_pdbg("%02x ", spi_blacklist[i]);
+		msg_pdbg(", size %i\n", spi_blacklist_size);
+	}
+	free(tmp);
+
+	tmp = extract_programmer_param("spi_ignorelist");
+	if (tmp) {
+		i = strlen(tmp);
+		if (!strncmp(tmp, "0x", 2)) {
+			i -= 2;
+			memmove(tmp, tmp + 2, i + 1);
+		}
+		if ((i > 512) || (i % 2)) {
+			msg_perr("Invalid SPI command ignorelist length\n");
+			free(tmp);
+			return 1;
+		}
+		spi_ignorelist_size = i / 2;
+		for (i = 0; i < spi_ignorelist_size * 2; i++) {
+			if (!isxdigit((unsigned char)tmp[i])) {
+				msg_perr("Invalid char \"%c\" in SPI command "
+					 "ignorelist\n", tmp[i]);
+				free(tmp);
+				return 1;
+			}
+		}
+		for (i = 0; i < spi_ignorelist_size; i++) {
+			unsigned int tmp2;
+			/* SCNx8 is apparently not supported by MSVC (and thus
+			 * MinGW), so work around it with an extra variable
+			 */
+			sscanf(tmp + i * 2, "%2x", &tmp2);
+			spi_ignorelist[i] = (uint8_t)tmp2;
+		}
+		msg_pdbg("SPI ignorelist is ");
+		for (i = 0; i < spi_ignorelist_size; i++)
+			msg_pdbg("%02x ", spi_ignorelist[i]);
+		msg_pdbg(", size %i\n", spi_ignorelist_size);
+	}
+	free(tmp);
 
 	/* frequency to emulate in Hz (default), KHz, or MHz */
 	tmp = extract_programmer_param("freq");
@@ -478,7 +557,7 @@ void dummy_chip_readn(const struct flashctx *flash, uint8_t *buf, const chipaddr
 static int emulate_spi_chip_response(const struct flashctx *flash, unsigned int writecnt, unsigned int readcnt,
 		      const unsigned char *writearr, unsigned char *readarr)
 {
-	unsigned int offs;
+	unsigned int offs, i;
 	static int unsigned aai_offs;
 	static int aai_active = 0;
 
@@ -486,7 +565,24 @@ static int emulate_spi_chip_response(const struct flashctx *flash, unsigned int 
 		msg_perr("No command sent to the chip!\n");
 		return 1;
 	}
-	/* TODO: Implement command blacklists here. */
+	/* spi_blacklist has precedence before spi_ignorelist. */
+	for (i = 0; i < spi_blacklist_size; i++) {
+		if (writearr[0] == spi_blacklist[i]) {
+			msg_pdbg("Refusing blacklisted SPI command 0x%02x\n",
+				 spi_blacklist[i]);
+			return SPI_INVALID_OPCODE;
+		}
+	}
+	for (i = 0; i < spi_ignorelist_size; i++) {
+		if (writearr[0] == spi_ignorelist[i]) {
+			msg_cdbg("Ignoring ignorelisted SPI command 0x%02x\n",
+				 spi_ignorelist[i]);
+			/* Return success because the command does not fail,
+			 * it is simply ignored.
+			 */
+			return 0;
+		}
+	}
 	switch (writearr[0]) {
 	case JEDEC_RES:
 		if (emu_chip != EMULATE_ST_M25P10_RES)
@@ -705,7 +801,7 @@ static int dummy_spi_send_command(const struct flashctx *flash, unsigned int wri
 	case EMULATE_VARIABLE_SIZE:
 		if (emulate_spi_chip_response(flash, writecnt, readcnt, writearr,
 					      readarr)) {
-			msg_perr("Invalid command sent to flash chip!\n");
+			msg_pdbg("Invalid command sent to flash chip!\n");
 			return 1;
 		}
 		break;
