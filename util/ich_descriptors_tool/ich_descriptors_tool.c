7f0476ad2f140b0600b9f90d6939058c7a664749
Patrick Georgi <pgeorgi@google.com>
2017-03-22 21:37:36 +0100

    Import current ich_descriptors_tool from upstream
    BUG=chromium:478356
    BRANCH=none
    TEST=still builds
    
    Change-Id: I267316f578f855807f130b3aad994fe55a601bae
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/458403
    Commit-Ready: Patrick Georgi <pgeorgi@chromium.org>
    Tested-by: Patrick Georgi <pgeorgi@chromium.org>
    Reviewed-by: Stefan Reinauer <reinauer@chromium.org>

--- util/ich_descriptors_tool/ich_descriptors_tool.c
+++ util/ich_descriptors_tool/ich_descriptors_tool.c
@@ -25,7 +25,6 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -33,8 +32,15 @@
 #include <string.h>
 #include <errno.h>
 #include "ich_descriptors.h"
+/* Some DJGPP builds define __unix__ although they don't support mmap().
+ * Cygwin defines __unix__ and supports mmap(), but it does not work well.
+ */
+#if !defined(__MSDOS__) && !IS_WINDOWS && (defined(unix) || defined(__unix__) || defined(__unix)) || (defined(__MACH__) && defined(__APPLE__))
+#define HAVE_MMAP 1
+#include <sys/mman.h>
+#endif
 
-static void dump_file(const char *basename, const uint32_t *dump, unsigned int len, struct ich_desc_region *reg, unsigned int i)
+static void dump_file(const char *prefix, const uint32_t *dump, unsigned int len, struct ich_desc_region *reg, unsigned int i)
 {
 	int ret;
 	char *fn;
@@ -48,8 +54,7 @@ static void dump_file(const char *basename, const uint32_t *dump, unsigned int l
 
 	reg_name = region_names[i];
 	if (base > limit) {
-		printf("The %s region is unused and thus not dumped.\n",
-		       reg_name);
+		printf("The %s region is unused and thus not dumped.\n", reg_name);
 		return;
 	}
 
@@ -62,22 +67,23 @@ static void dump_file(const char *basename, const uint32_t *dump, unsigned int l
 		return;
 	}
 
-	fn = malloc(strlen(basename) + strlen(reg_name) + strlen(".bin") + 2);
+	fn = malloc(strlen(prefix) + strlen(reg_name) + strlen(".bin") + 2);
 	if (!fn) {
 		fprintf(stderr, "Out of memory!\n");
 		exit(1);
 	}
-	snprintf(fn, strlen(basename) + strlen(reg_name) + strlen(".bin") + 2,
-		 "%s.%s.bin", basename, reg_name);
+	snprintf(fn, strlen(prefix) + strlen(reg_name) + strlen(".bin") + 2,
+		 "%s.%s.bin", prefix, reg_name);
 	printf("Dumping %u bytes of the %s region from 0x%08x-0x%08x to %s... ",
 	       file_len, region_names[i], base, limit, fn);
 	int fh = open(fn, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-	free(fn);
 	if (fh < 0) {
 		fprintf(stderr,
 			"ERROR: couldn't open(%s): %s\n", fn, strerror(errno));
+		free(fn);
 		exit(1);
 	}
+	free(fn);
 
 	ret = write(fh, &dump[base >> 2], file_len);
 	if (ret != file_len) {
@@ -89,12 +95,12 @@ static void dump_file(const char *basename, const uint32_t *dump, unsigned int l
 	close(fh);
 }
 
-void dump_files(const char *n, const uint32_t *buf, unsigned int len, struct ich_desc_region *reg)
+void dump_files(const char *name, const uint32_t *buf, unsigned int len, struct ich_desc_region *reg)
 {
 	unsigned int i;
 	printf("=== Dumping region files ===\n");
 	for (i = 0; i < 5; i++)
-		dump_file(n, buf, len, reg, i);
+		dump_file(name, buf, len, reg, i);
 	printf("\n");
 }
 
@@ -107,14 +113,17 @@ static void usage(char *argv[], char *error)
 "where <image file name> points to an image of the contents of the SPI flash.\n"
 "In case the image is really in descriptor mode %s\n"
 "will pretty print some of the contained information.\n"
-"To also print the data stored in the descriptor strap you have to indicate\n"
+"To also print the data stored in the descriptor straps you have to indicate\n"
 "the chipset series with the '-c' parameter and one of the possible arguments:\n"
 "\t- \"ich8\",\n"
 "\t- \"ich9\",\n"
 "\t- \"ich10\",\n"
+"\t- \"silvermont\" for chipsets from Intel's Silvermont architecture (e.g. Bay Trail),\n"
 "\t- \"5\" or \"ibex\" for Intel's 5 series chipsets,\n"
 "\t- \"6\" or \"cougar\" for Intel's 6 series chipsets,\n"
 "\t- \"7\" or \"panther\" for Intel's 7 series chipsets.\n"
+"\t- \"8\" or \"lynx\" for Intel's 8 series chipsets.\n"
+"\t- \"9\" or \"wildcat\" for Intel's 9 series chipsets.\n"
 "If '-d' is specified some regions such as the BIOS image as seen by the CPU or\n"
 "the GbE blob that is required to initialize the GbE are also dumped to files.\n",
 	argv[0], argv[0]);
@@ -151,8 +160,7 @@ int main(int argc, char *argv[])
 		}
 	}
 	if (fn == NULL)
-		usage(argv,
-		      "Need a file name of a descriptor image to read from.");
+		usage(argv, "Need the file name of a descriptor image to read from.");
 
 	fd = open(fn, O_RDONLY);
 	if (fd < 0)
@@ -161,17 +169,18 @@ int main(int argc, char *argv[])
 	if (len < 0)
 		usage(argv, "Seeking to the end of the file failed");
 
-	buf = mmap(NULL, len, PROT_READ, MAP_PRIVATE, fd, 0);
-	if (buf == (void *) -1) {
+#ifdef HAVE_MMAP
+	buf = (uint32_t *)mmap(NULL, len, PROT_READ, MAP_PRIVATE, fd, 0);
+	if (buf == (void *) -1)
+#endif
+	{
 		/* fallback for stupid OSes like cygwin */
-		int ret;
 		buf = malloc(len);
 		if (!buf)
 			usage(argv, "Could not allocate memory");
 		lseek(fd, 0, SEEK_SET);
-		ret = read(fd, buf, len);
-		if (ret != len)
-			usage(argv, "Seeking to the end of the file failed");
+		if (len != read(fd, buf, len))
+			usage(argv, "Reading the descriptor image file failed");
 	}
 	printf("The flash image has a size of %d [0x%x] bytes.\n", len, len);
 	close(fd);
@@ -192,6 +201,14 @@ int main(int argc, char *argv[])
 		else if ((strcmp(csn, "7") == 0) ||
 			 (strcmp(csn, "panther") == 0))
 			cs = CHIPSET_7_SERIES_PANTHER_POINT;
+		else if ((strcmp(csn, "8") == 0) ||
+			 (strcmp(csn, "lynx") == 0))
+			cs = CHIPSET_8_SERIES_LYNX_POINT;
+		else if ((strcmp(csn, "silvermont") == 0))
+			cs = CHIPSET_BAYTRAIL;
+		else if ((strcmp(csn, "9") == 0) ||
+			 (strcmp(csn, "wildcat") == 0))
+			cs = CHIPSET_9_SERIES_WILDCAT_POINT;
 	}
 
 	ret = read_ich_descriptors_from_dump(buf, len, &desc);
@@ -202,8 +219,7 @@ int main(int argc, char *argv[])
 		printf("Image not in descriptor mode.\n");
 		exit(1);
 	case ICH_RET_OOB:
-		printf("Tried to access a location out of bounds of the image. "
-		       "- Corrupt image?\n");
+		printf("Tried to access a location out of bounds of the image. - Corrupt image?\n");
 		exit(1);
 	default:
 		printf("Unhandled return value at %s:%u, please report this.\n",
